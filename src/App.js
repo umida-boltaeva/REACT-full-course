e085d85d70f47b3212db2fb3e3bccbe6a325b984
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-06-05 11:01:43 +1000

    output dynamic content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
+import Person from "./Person/Person";
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <h1>Hey, I'm a React App</h1>
-  //   </div>
-  // );
-  return React.createElement("div", null, React.createElement("h1", {className: "App"}, "Is it working now?"));
+  return (
+    <div className="App">
+      <h1>Hey, I'm a React App</h1>
+      <p>This is definitely working!</p>
+      <Person name="Max" age="22" />
+      <Person name="Umida" age="25">My hobby is racing.</Person>
+      <Person name="Baha" age="25"/>
+    </div>
+  );
+  
 }
 
 export default App;
