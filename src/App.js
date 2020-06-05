e73c21e66fb73667cdecae01e88da65db233d28c
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-06-05 10:36:27 +1000

    understand JSX code

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hey, I'm a React App</h1>
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     <h1>Hey, I'm a React App</h1>
+  //   </div>
+  // );
+  return React.createElement("div", null, React.createElement("h1", {className: "App"}, "Is it working now?"));
 }
 
 export default App;
