1275ccc04763bc9f709323815ecd2e2ac752afd4
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-06-10 10:48:54 +1000

    passing method referances between components

--- src/App.js
+++ src/App.js
@@ -1,42 +1,45 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from "./Person/Person";
 
-const App = props => {
-  const  [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       {name: "Max", age: 20},
       {name: "Umida", age: 25},
       {name: "Baha", age: 25}
     ],
     otherState: "some other value"
-  });
+  };
 
-console.log(personsState);
-
-  const switchNameHandler = () => {
+ switchNameHandler = (newName) => {
     //console.log("was clicked");
-    setPersonsState({
+    this.setState({
       persons: [
-        {name: "Maxima", age: 20},
+        {name: newName, age: 20},
         {name: "Umida", age: 25},
         {name: "Baha", age: 30}
       ]
     })
   }
   
+  render(){
     return (
       <div className="App">
         <h1>Hey, I'm a React App</h1>
         <p>This is definitely working!</p>
-        <button onClick={switchNameHandler}>Switch Name</button>
-        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My hobby is racing.</Person>
-        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+        <button onClick={this.switchNameHandler.bind(this, "Steve")}>Switch Name</button>
+        <Person name={this.state.persons[0].name}
+         age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name}
+         age={this.state.persons[1].age}
+         click={this.switchNameHandler.bind(this, "Mimic")}>My hobby is racing.</Person>
+        <Person name={this.state.persons[2].name}
+         age={this.state.persons[2].age}/>
       </div>
-    ); 
-}
+    );
+  }  
+};
 
 export default App;
-
 
