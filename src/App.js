b9594d58ad9e0a2714a5e2a8bbb57b01c4551264
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-06-12 11:15:11 +1000

    outputting lists

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ class App extends Component {
  switchNameHandler = (newName) => {
     this.setState({
       persons: [
-        {name: newName, age: 20},
-        {name: "Umida", age: 25},
+        {name: "Max", age: 20},
+        {name: newName, age: 25},
         {name: "Baha", age: 30}
       ]
     })
@@ -53,20 +53,14 @@ class App extends Component {
     if(this.state.showPersons) {
       persons = (
         <div>
-              <Person 
-                name={this.state.persons[0].name}
-                age={this.state.persons[0].age} />
-              <Person 
-                name={this.state.persons[1].name}
-                age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, "Mimic")}
-                changed={this.nameChangeHandler}>My hobby is racing.</Person>
-              <Person 
-                name={this.state.persons[2].name}
-                age={this.state.persons[2].age}/>
-            </div>
+        {this.state.persons.map(person => {
+          return <Person
+                  name={person.name}
+                  age={person.age} />
+        })}
+        </div>
       );
-    };
+    }
 
     return (
       <div className="App">
