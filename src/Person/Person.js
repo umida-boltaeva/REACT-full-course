d15afd754f3daf423a61e4e3278bb6b99ad3a28f
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-06-20 23:07:35 +1000

    CSS modules and media queries

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,27 +1,13 @@
 import React from "react";
-import styled from "styled-components";
-// import "./Person.css";
-
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: 16px auto;
-    border: 2px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
-
-    @media (min-width: 500px){
-        width: 450px;
-    }
-`;
+import styles from"./Person.css";
 
 const person = (props) => {
     return (
-        <StyledDiv>
+        <div className={styles.Person}>
             <p onClick={props.click}>I am {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
-        </StyledDiv>
+        </div>
 
     )
     
