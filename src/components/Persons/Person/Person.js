b104923c11115149cff49f1601798f0156e81d56
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-06-25 17:22:27 +1000

    use shouldComponentUpdate for optimization

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, {Component} from "react";
 import styles from"./Person.css";
 
-const person = (props) => {
-    return (
-        <div className={styles.Person}>
-            <p onClick={props.click}>I am {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name}/>
-        </div>
 
-    )
+
+class Person extends Component {
+    render() {
+        return (
+            <div className={styles.Person}>
+                <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+            </div>
+    
+        )
+    }
+    
     
 };
 
-export default person;
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, {Component} from "react";
 import Person from "./Person/Person"
 
-const Persons = (props) => {
-    return (
-        props.persons.map((person, index) => <Person
-                click={() => props.clicked(index)}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                changed={(event) => props.changed(event, person.id)} />
-        )
-    );
+class Persons extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextProps.persons !== this.props.persons) {
+            return true;
+        }else {
+            return false;
+        }
+        // return true;
+    }    
+ 
+    render () {
+        return (
+            this.props.persons.map((person, index) => <Person
+                    click={() => this.props.clicked(index)}
+                    name={person.name}
+                    age={person.age}
+                    key={person.id}
+                    changed={(event) => this.props.changed(event, person.id)} />
+            )
+        );
+    }
+    
 }
 
 export default Persons;
