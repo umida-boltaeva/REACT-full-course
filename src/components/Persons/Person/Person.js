d6f1333bea721e5deff5b1ef2f26b1351e427aa7
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-07-01 16:29:55 +1000

    render adjacent JSX elements

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,14 +5,14 @@ import styles from"./Person.css";
 
 class Person extends Component {
     render() {
-        return (
-            <div className={styles.Person}>
-                <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} years old!</p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}/>
-            </div>
-    
-        )
+        return [          
+            <p key="k1" onClick={this.props.click}>
+            I am {this.props.name} and I am {this.props.age} years old!</p>,
+            <p key="k2">{this.props.children}</p>,
+            <input key="k3" type="text" onChange={this.props.changed} 
+            value={this.props.name}/>
+           
+        ];
     }
     
     
