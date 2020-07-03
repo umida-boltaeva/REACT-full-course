4d511b5399c5d84692e6a41cf23dfa67d291379a
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-07-03 16:40:06 +1000

    pass unknown props

--- src/HOC/WithClass.js
+++ src/HOC/WithClass.js
@@ -3,7 +3,7 @@ import React from "react";
  const withClass = (WrappedComponent, styleName) => {
     return props => (
         <div className={styleName}>
-         <WrappedComponent />
+         <WrappedComponent {...props}/>
      </div>
      );
  };
--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,21 +1,21 @@
 import React, {Component} from "react";
 import styles from"./Person.css";
-import WithClass from "../../../HOC/WithClass";
+import withClass from "../../../HOC/withClass";
 
 
 
 class Person extends Component {
     render() {
         return (
-           <WithClass classes={styles.Person}>
+           <>
                 <p onClick={this.props.click}>
                 I am {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} 
                 value={this.props.name}/>
-           </WithClass>
+           </>
         );
     }   
 };
 
-export default Person;
+export default withClass(Person, styles.Person);
