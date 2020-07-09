82f723eb0a8fc48fe095c7bd24d8225cb9a03faf
umida-boltaeva <umidakhon.boltaeva@gmail.com>
2020-07-09 19:21:58 +1000

    using PropTypes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import PropTypes from 'prop-types';
 import styles from"./Person.css";
 import withClass from "../../../HOC/withClass";
 
@@ -16,6 +17,13 @@ class Person extends Component {
            </>
         );
     }   
+};
+
+Person.propTypes= {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
 };
 
 export default withClass(Person, styles.Person);
